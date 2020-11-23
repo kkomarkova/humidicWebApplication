a22fcbab79210073e2c3e22ed76872da17db3d10
jakubmraz <58209778+jakubmraz@users.noreply.github.com>
2020-11-23 10:05:21 +0100

    Created the HumidityReport class

---
+++ src/js/HumidityReport.ts
@@ -0,0 +1,13 @@
+export class HumidityReport{
+    humidity:number;
+    time:Date;
+
+    constructor(humidity:number, time:Date){
+        this.humidity = humidity;
+        this.time = time;
+    }
+
+    DebugReport():string{
+        return "The humidity at " + this.time.toLocaleString() + "was " + this.humidity + "%." 
+    }
+}
